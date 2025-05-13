7f20bb2fdae70ecfed66b93b5a04a98fd36fb88b
Joshua Evans <thejoshuaevans@gmail.com>
2025-05-13 14:24:52 -0700

    Story Bible File - Separated the story bible into its own file

--- plot_overview.py
+++ plot_overview.py
@@ -5,6 +5,7 @@ from openai.types.responses import ResponseTextDeltaEvent
 from agents import Agent, Runner, trace
 
 from src.agents.plot_overview_agent import PlotOverviewAgent
+from src.assets.story_bible import story_bible
 
 def output_stream_text(text: str) -> None:
     print(text, end="", flush=True)
@@ -13,14 +14,7 @@ async def main():
     agent = PlotOverviewAgent()
 
     print('Initializing agent...')
-    initialize_result = await agent.initialize(story_bible="""
-Characters:
-- Jumpy: A well-aged but still sprightly rabbit who use to have a tendency to slack off whenever the opportunity arose. He is now a wise old rabbit who has learned the value of hard work and responsibility.
-- Lumber: An ancient tortoise who has always been a hard and diligent worker. He is now a wise old tortoise who has learned the value of taking breaks and enjoying life.
-
-Location:
-The forest: A lush and vibrant forest filled with trees, flowers, and wildlife. It is a place of beauty and tranquility. A path runs through the forest that the forest animals use for impromptu races
-""", streaming_cb=output_stream_text)
+    initialize_result = await agent.initialize(story_bible, streaming_cb=output_stream_text)
     print('\n')
     print('Agent initial response: ' + initialize_result)
     print('\n')
---
+++ src/assets/story_bible.py
@@ -0,0 +1,8 @@
+story_bible = """
+Characters:
+- Jumpy: A well-aged but still sprightly rabbit who use to have a tendency to slack off whenever the opportunity arose. He is now a wise old rabbit who has learned the value of hard work and responsibility.
+- Lumber: An ancient tortoise who has always been a hard and diligent worker. He is now a wise old tortoise who has learned the value of taking breaks and enjoying life.
+
+Location:
+The forest: A lush and vibrant forest filled with trees, flowers, and wildlife. It is a place of beauty and tranquility. A path runs through the forest that the forest animals use for impromptu races
+"""
